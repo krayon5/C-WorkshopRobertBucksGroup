550841d06b2a84c9f08622db31cef5b14652cca2
krayon5 <135983399+krayon5@users.noreply.github.com>
2023-06-13 12:18:56 +0100

    Day1
    aaa

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices;
+using System.Xml;
+
+Console.WriteLine("Hello, World!");
+
+//create a string variable
+
+string myName = "Sara";
+
+//more variables:
+
+string superHeroName = "aot";
+
+Console.WriteLine(superHeroName);
+
+
+int myAge = 40;
+
+Console.WriteLine(myAge);
+
+
+decimal myMoney = 100.50m;
+
+Console.WriteLine(myMoney);
+
+
+double myDouble = 100.50;
+
+Console.WriteLine(myDouble);
+
+
+bool isRaining  = true;
+
+Console.WriteLine(isRaining);
+
+
+//output
+
+string outputString = $"This thursday, i will be watching {superHeroName}";
+Console.WriteLine(outputString);
+
+ 
