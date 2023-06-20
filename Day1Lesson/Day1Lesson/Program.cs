6b738a32b1605b35b9fdd63eb2e834acd40bf512
krayon5 <135983399+krayon5@users.noreply.github.com>
2023-06-20 11:56:26 +0100

    Day3
    a

--- Day1Lesson/Day1Lesson/Program.cs
+++ Day1Lesson/Day1Lesson/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection.Metadata;
 using System.Runtime.InteropServices;
 using System.Xml;
 
@@ -40,4 +41,75 @@ Console.WriteLine(isRaining);
 string outputString = $"This thursday, i will be watching {superHeroName}";
 Console.WriteLine(outputString);
 
+//Creating and calling a function
+void functionType1()
+{
+    string football_player = "Messi";
+    Console.WriteLine(football_player);
+}
 
+functionType1();
+
+void functionType2(int goals)
+{
+    string aaa = $"Massi has {goals} lifetime goals";
+    Console.WriteLine (aaa);
+}
+functionType2(807);
+
+//more than one parameter
+
+void functionType2b(int one, string two, bool three)
+{
+    string Output2b = $"The values are {one}, {two}, {three}";
+    Console.WriteLine(Output2b);
+}
+
+int one = 1;
+string two = "Yes";
+bool three = true;
+
+functionType2b(one, two, three);
+
+//Type Three
+//Function takes parameters
+
+int functionThatAddsTwoNumbers(int numberOne, int numberTwo)
+{
+    int sum = numberOne + numberTwo;
+    return sum;
+}
+
+int doubleoseven = 007;
+int randomAgent = 50;
+
+//function call 
+
+int result = functionThatAddsTwoNumbers(doubleoseven, randomAgent);
+
+string outputOfAgents = $"The sum of {doubleoseven} and {randomAgent} is {result}";
+Console.WriteLine(outputOfAgents);
+
+//Type Four
+
+//a function that is NOT taking any parameters
+//and returning a value
+
+//function definition
+int FunctionThatReturnsRandomNumber()
+{
+    Random random = new Random();
+    int randomNumber = random.Next(1, 100);
+    return randomNumber;
+}
+
+//function call
+//i simply want a random number, between 1 and 100
+
+int randomnumber = FunctionThatReturnsRandomNumber();
+string somerandomsentence = $"The random number is {randomnumber} between 1 and 100, no parameters are sent";
+
+//i want a random number betwen 500 and 800
+int lowerboundvalue = 500;
+int upperboundvalue = 800;
+int randomnumber2 = FunctionThatReturnsRandomNumber(500, 800);
